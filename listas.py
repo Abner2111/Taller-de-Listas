95a844c945bee1ad02495ac37a878d4eba6d5e57
[Abner] <[abnerAQ73@gmail.com]>
2018-05-23 08:50:13 -0600

    printL y appe funcionan

--- listas.py
+++ listas.py
@@ -1,8 +1,12 @@
+
 class Nodo:
     def __init__(self, valor = None):
         self.next = None # puntero al nodo siguiente
         self.prev = None # puntero al nodo anterior
         self.valor = valor
+
+    def __str__(self):
+        return str(self.valor)
 class ListaDoble:
     def __init__(self):
         self.head = None # puntero al inicio de la lista
@@ -10,23 +14,46 @@ class ListaDoble:
         self.largo = 0
 
     def appe(self, dato):
-        self.largo += 1
-        if self.head == None:
-            self.head = Nodo(valor = dato)
+        if isinstance(dato, int):
+            self.largo += 1
+            if self.head == None:
+                self.head = Nodo(valor = dato)
+            else:
+                tmp = self.head
+                while tmp.next != None:
+                    tmp = tmp.next
+                tmp.next = Nodo(valor = dato)
+                tmp.next.prev = tmp
+                self.tail = tmp.next
         else:
-            tmp = self.head
-            while tmp.next != None:
-                tmp = tmp.next
-            tmp.next = Nodo(valor = dato)
-            self.tail = temp.next
-            tmp.next.prev = tmp
+            print("ERROR")
+
+    def printL(self):
+
+        nodo = self.head
+        lista = "["
+        while nodo != None:
+            if nodo.next != None:
+                lista += nodo.__str__()
+                lista += ", "
+            else:
+                lista += nodo.__str__()
+            nodo = nodo.next
+        lista += "]"
+        print(lista)
 
     def reve(self):
         nodo = self.tail
         while nodo != None:
             print(nodo.__str__())
             nodo = nodo.prev
-            
-    def toList():
-    
-        
+
+
+
+l1 = ListaDoble()
+l1.appe(12)
+l1.appe(13)
+l1.appe(14)
+l1.appe(15)
+
+l1.printL()
