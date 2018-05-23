c72d3f4a414942ae5203f348a3e678958f0925b3
Curso <curso@CE-05.ec.tec.ac.cr>
2018-05-23 09:20:56 -0600

    Funciones terminadas 2

--- listas.py
+++ listas.py
@@ -1,10 +1,13 @@
+
 class Nodo:
     def __init__(self, valor = None):
         self.next = None # puntero al nodo siguiente
         self.prev = None # puntero al nodo anterior
         self.valor = valor
+        
     def get_valor(self):
         return self.valor
+    
     def __str__(self):
         return str(self.valor)
     
@@ -15,16 +18,33 @@ class ListaDoble:
         self.largo = 0
 
     def appe(self, dato):
-        self.largo += 1
-        if self.head == None:
-            self.head = Nodo(valor = dato)
+        if isinstance(dato, int):
+            self.largo += 1
+            if self.head == None:
+                self.head = Nodo(valor = dato)
+            else:
+                tmp = self.head
+                while tmp.next != None:
+                    tmp = tmp.next
+                tmp.next = Nodo(valor = dato)
+                tmp.next.prev = tmp
+                self.tail = tmp.next
         else:
-            tmp = self.head
-            while tmp.next != None:
-                tmp = tmp.next
-            tmp.next = Nodo(valor = dato)
-            self.tail = tmp.next
-            tmp.next.prev = tmp
+            print("ERROR")
+
+    def printL(self):
+
+        nodo = self.head
+        lista = "["
+        while nodo != None:
+            if nodo.next != None:
+                lista += nodo.__str__()
+                lista += ", "
+            else:
+                lista += nodo.__str__()
+            nodo = nodo.next
+        lista += "]"
+        print(lista)
 
     def reve(self):
         nodo = self.tail
@@ -41,6 +61,7 @@ class ListaDoble:
             nodo = nodo.next
         return ListaN
 
+
 Prueba = ListaDoble()
 Prueba.appe(1)
 Prueba.appe(2)
@@ -49,7 +70,4 @@ Prueba.appe(4)
 Prueba.appe(5)
 Prueba.appe(6)
 
-Prueba.toList()
-
-print(Prueba.toList()[2])
-
+print(Prueba.toList())
